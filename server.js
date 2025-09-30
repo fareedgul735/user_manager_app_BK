0378c2433c0cba58a5b46c55acf380c7961a650f
fareedgul <fareedgul735@gmail.com>
2025-09-29 22:46:20 -0700

    push/changes

--- server.js
+++ server.js
@@ -7,27 +7,31 @@ import userRouter from "./router/userRouter.js";
 dotenv.config();
 
 const app = express();
-const port = 5000;
-const localHost = "localhost";
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://user-manager-app-lilac.vercel.app",
+    origin: [
+      "http://localhost:3000",
+      "https://user-manager-app-lilac.vercel.app",
+    ],
     credentials: true,
   })
 );
 app.use(bodyParser.json());
 
 app.use("/users", userRouter);
+
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("mongdb_connected");
-    app.listen(port, localHost, () => {
-      console.log(`server is on ${localHost} ${port}`);
+    console.log("MongoDB connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   } catch (err) {
-    console.log(err);
+    console.error("DB connection error:", err);
   }
 };
 
